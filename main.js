fd2e0df07dcc1f4197011763af6e5781206d2d1d
devdan <oluochdancun19@gmail.com>
2022-06-18 10:33:03 +0300

    commit tree b69b15cb2d5e0dafa0d0606475729431c7dab3f8

--- main.js
+++ main.js
@@ -5,7 +5,35 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const error = document.getElementById("modal")
+error.className+="hidden"
 
+const like = document.querySelectorAll(".like-glyph")
+
+function heartLiker(e) {
+  const liker = e.target;
+  mimicServerCall()
+    .then(function(){
+      if ( liker.innerText === EMPTY_HEART) {
+        liker.innerText = FULL_HEART;
+        liker.className = "activated-heart";
+      } else {
+        liker.innerText = EMPTY_HEART;
+        liker.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+for (const likers in like) {
+  likers.addEventListener("click", heartLiker);
+}
+// like.addEventListener("click", mimicServerCall)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
